371fc4062629ef739113b71ec9c9fb3427c22c5a
Eldesoky, Ramy <Ramy_Eldesoky@McAfee.com>
2019-06-08 08:58:24 +0200

    Implement fontawesome

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,8 +1,30 @@
 import React from "react";
 import "./Sidebar.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+	faSearch,
+	faPaperPlane,
+	faPlay,
+	faBookmark
+} from "@fortawesome/free-solid-svg-icons";
 
 function Sidebar(props) {
-	return <div className="Sidebar">Search</div>;
+	return (
+		<div className="Sidebar">
+			<div>
+				<FontAwesomeIcon icon={faSearch} />
+			</div>
+			<div>
+				<FontAwesomeIcon icon={faPaperPlane} />
+			</div>
+			<div>
+				<FontAwesomeIcon icon={faPlay} />
+			</div>
+			<div>
+				<FontAwesomeIcon icon={faBookmark} />
+			</div>
+		</div>
+	);
 }
 
 export default Sidebar;
