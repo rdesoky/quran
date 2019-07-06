25d0a10e7a0080ce32870784aa0a99b7393c60d3
Eldesoky, Ramy <Ramy_Eldesoky@McAfee.com>
2019-07-06 13:49:35 +0200

    Updates

---
+++ precache-manifest.aed22866374416423f68edf02ec76d2d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1095e2832847f94e0a94fe35a92a2b4e",
+    "url": "/muslim-web/index.html"
+  },
+  {
+    "revision": "4eefffb27688627792bd",
+    "url": "/muslim-web/static/css/main.1c95a8b6.chunk.css"
+  },
+  {
+    "revision": "9ff755efb1599523aed2",
+    "url": "/muslim-web/static/js/2.a827e4af.chunk.js"
+  },
+  {
+    "revision": "4eefffb27688627792bd",
+    "url": "/muslim-web/static/js/main.f57cec9a.chunk.js"
+  },
+  {
+    "revision": "72546e99d15111639626",
+    "url": "/muslim-web/static/js/runtime~main.e735c96e.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "https://rdesoky.github.io/muslim-web/precache-manifest.e6e6f094a86858b3829d52e606049cba.js"
+  "/muslim-web/precache-manifest.aed22866374416423f68edf02ec76d2d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("https://rdesoky.github.io/muslim-web/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/muslim-web/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
