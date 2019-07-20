1f2bc20118a952c426c4a295717f454cd7b33f48
Eldesoky, Ramy <Ramy_Eldesoky@McAfee.com>
2019-07-20 10:05:47 +0200

    Implement tafseer selector

--- src/components/Modal/Tafseer.js
+++ src/components/Modal/Tafseer.js
@@ -7,8 +7,16 @@ import { withAppContext } from "../../context/App";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faAngleLeft, faAngleRight } from "@fortawesome/free-solid-svg-icons";
 
+const TafseerList = [
+	{ id: "muyassar", name: "الميسر", dir: "rtl", file: "ar.muyassar.txt" },
+	{ id: "yusufali", name: "English", dir: "ltr", file: "en.yusufali.txt" }
+];
+
 const Tafseer = ({ onClose, isOpen, appContext }) => {
-	const [tafseer, setTafseer] = useState([]);
+	const [tafseer, setTafseer] = useState(
+		localStorage.getItem("tafseer") || "muyassar"
+	);
+	const [tafseerData, setTafseerData] = useState([]);
 
 	const handleKeyDown = e => {
 		switch (e.code) {
@@ -31,17 +39,17 @@ const Tafseer = ({ onClose, isOpen, appContext }) => {
 	};
 
 	useEffect(() => {
-		//document.querySelector(".CancelButton").focus();
-		fetch(`${process.env.PUBLIC_URL}/ar.muyassar.txt`)
+		let fileName = TafseerList.find(i => i.id === tafseer).file;
+		fetch(`${process.env.PUBLIC_URL}/${fileName}`)
 			.then(r => r.text())
 			.then(txt => {
-				setTafseer(txt.split("\n"));
+				setTafseerData(txt.split("\n"));
 			});
 		document.addEventListener("keydown", handleKeyDown);
 		return () => {
 			document.removeEventListener("keydown", handleKeyDown);
 		};
-	}, []);
+	}, [tafseer]);
 
 	const renderVerse = () => {
 		const verseList = appContext.verseList();
@@ -50,13 +58,39 @@ const Tafseer = ({ onClose, isOpen, appContext }) => {
 		}
 	};
 	const renderTafseer = () => {
-		if (tafseer.length > appContext.selectStart) {
-			return tafseer[appContext.selectStart];
+		if (tafseerData.length > appContext.selectStart) {
+			return tafseerData[appContext.selectStart];
 		}
 		return "Loading...";
 	};
 
+	const onSelectTafseer = e => {
+		const { target: option } = e;
+		const tafseer = option.value;
+		localStorage.setItem("tafseer", option.value);
+		setTafseer(tafseer);
+	};
+
+	const renderSelector = () => {
+		return (
+			<select onChange={onSelectTafseer}>
+				{TafseerList.map(taf => {
+					return (
+						<option value={taf.id} selected={taf.id === tafseer}>
+							{taf.name}
+						</option>
+					);
+				})}
+			</select>
+		);
+	};
+
+	const tafDirection = () => {
+		return TafseerList.find(i => i.id === tafseer).dir;
+	};
+
 	const ayaInfo = QData.ayaIdInfo(appContext.selectStart);
+
 	return (
 		<Modal open={isOpen} onClose={onClose}>
 			<div className="Title">
@@ -74,12 +108,16 @@ const Tafseer = ({ onClose, isOpen, appContext }) => {
 					<FontAwesomeIcon icon={faAngleLeft} />
 				</button>
 			</div>
+
 			<div>
 				<p className="TafseerVerse">{renderVerse()}</p>
 			</div>
-			<div className="TafseerSelector">Select Tafseer</div>
 			<div>
-				<p className="TafseerText">{renderTafseer()}</p>
+				<p className="TafseerText" style={{ direction: tafDirection() }}>
+					{renderSelector()}
+					{" - "}
+					{renderTafseer()}
+				</p>
 			</div>
 		</Modal>
 	);
