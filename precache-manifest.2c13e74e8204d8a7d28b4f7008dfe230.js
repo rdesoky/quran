e810b307173f9ba9c662ab74e83d9c99e4a28060
Ramy (Home Mac) <rdesoky@gmail.com>
2019-09-07 06:19:08 -0700

    Updates

---
+++ precache-manifest.2c13e74e8204d8a7d28b4f7008dfe230.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54fcc694efaf201102c134455f226a52",
+    "url": "/quran/index.html"
+  },
+  {
+    "revision": "5f9c2fb8038a2162f8a8",
+    "url": "/quran/static/css/main.88fd2744.chunk.css"
+  },
+  {
+    "revision": "1f3b8c6df46224f6a891",
+    "url": "/quran/static/js/2.f57a2d16.chunk.js"
+  },
+  {
+    "revision": "5f9c2fb8038a2162f8a8",
+    "url": "/quran/static/js/main.e3c67571.chunk.js"
+  },
+  {
+    "revision": "5eb79085e85e295b3b3e",
+    "url": "/quran/static/js/runtime~main.72f9a983.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/quran/precache-manifest.c6f099316af4a6434c1142cdce0c3288.js"
+  "/quran/precache-manifest.2c13e74e8204d8a7d28b4f7008dfe230.js"
 );
 
 self.addEventListener('message', (event) => {
