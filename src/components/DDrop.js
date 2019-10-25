65207de2951ea3c88379f5c1bfa730cc02e511f4
Eldesoky, Ramy <Ramy_Eldesoky@McAfee.com>
2019-10-25 15:31:04 -0700

    Support touch drag and drop

--- src/components/DDrop.js
+++ src/components/DDrop.js
@@ -1,57 +1,81 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 const DDrop = ({ children, onDrop, maxShift }) => {
-    const [captured, setCaptured] = useState(false);
-    const [X, setX] = useState(0);
-    const [Y, setY] = useState(0);
-    const [dX, setDX] = useState(0);
-    const [dY, setDY] = useState(0);
-
-    const onTouchStart = e => {
-        onMouseDown(e);
-        e.preventDefault();
-    };
-    const onMouseDown = ({ target, clientX, clientY, pointerId }) => {
-        if (pointerId) {
-            target.setPointerCapture(pointerId);
-        }
-        setCaptured(true);
-        setX(clientX);
-        setY(clientY);
-        setDX(0);
-        setDY(0);
-    };
-    const onMouseMove = ({ clientX, clientY }) => {
-        if (captured) {
-            if (Math.abs(clientX - X) < maxShift) {
-                setDX(clientX - X);
-            }
-            if (Math.abs(clientY - Y) < maxShift) {
-                setDY(clientY - Y);
-            }
-        }
-    };
-    const onMouseUp = ({ target, pointerId }) => {
-        if (pointerId) {
-            target.releasePointerCapture(pointerId);
-        }
-        setCaptured(false);
-        onDrop({ dX, dY });
-        setDX(0);
-        setDY(0);
-    };
-    return (
-        <div
-            onTouchStart={onTouchStart}
-            onTouchMove={onMouseMove}
-            onTouchEnd={onMouseUp}
-            onPointerDown={onMouseDown}
-            onPointerMove={onMouseMove}
-            onPointerUp={onMouseUp}
-        >
-            {children({ dX, dY })}
-        </div>
-    );
+	const [captured, setCaptured] = useState(false);
+	const [startX, setStartX] = useState(0);
+	const [startY, setStartY] = useState(0);
+	const [dX, setDX] = useState(0);
+	const [dY, setDY] = useState(0);
+	let rootElement;
+	const onTouchStart = e => {
+		const { clientX, clientY } = e.targetTouches[0];
+		const { target } = e;
+		onMouseDown({ clientX, clientY, target });
+		// e.preventDefault();
+	};
+	const onTouchMove = e => {
+		const { clientX, clientY } = e.targetTouches[0];
+		const { target } = e;
+		onMouseMove({ clientX, clientY, target });
+		e.preventDefault();
+	};
+	const onMouseDown = ({ target, clientX, clientY, pointerId }) => {
+		if (pointerId) {
+			target.setPointerCapture(pointerId);
+		}
+		setCaptured(true);
+		setStartX(clientX);
+		setStartY(clientY);
+		setDX(0);
+		setDY(0);
+	};
+	const onMouseMove = e => {
+		const { clientX, clientY } = e;
+		if (captured) {
+			if (Math.abs(clientX - startX) < maxShift) {
+				setDX(clientX - startX);
+			}
+			if (Math.abs(clientY - startY) < maxShift) {
+				setDY(clientY - startY);
+			}
+		}
+	};
+	const onMouseUp = ({ target, pointerId }) => {
+		if (pointerId) {
+			target.releasePointerCapture(pointerId);
+		}
+		setCaptured(false);
+		onDrop({ dX, dY });
+		setDX(0);
+		setDY(0);
+	};
+	useEffect(() => {
+		rootElement.addEventListener("touchstart", onTouchStart, {
+			passive: false
+		});
+		rootElement.addEventListener("touchmove", onTouchMove, { passive: false });
+		rootElement.addEventListener("touchend", onMouseUp, { passive: false });
+		return () => {
+			rootElement.removeEventListener("touchstart", onTouchStart);
+			rootElement.removeEventListener("touchmove", onTouchMove);
+			rootElement.removeEventListener("touchend", onMouseUp);
+		};
+	}, []);
+	return (
+		<div
+			ref={ref => {
+				rootElement = ref;
+			}}
+			// onTouchStart={onTouchStart}
+			// onTouchMove={onMouseMove}
+			// onTouchEnd={onMouseUp}
+			onPointerDown={onMouseDown}
+			onPointerMove={onMouseMove}
+			onPointerUp={onMouseUp}
+		>
+			{children({ dX, dY })}
+		</div>
+	);
 };
 
 export default DDrop;
