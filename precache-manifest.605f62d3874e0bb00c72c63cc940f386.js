c80fe6ccf3f6b67b31473f7bf8b96d4176008045
Ramy (Home PC) <rdesoky@gmail.com>
2019-11-14 07:55:46 -0800

    Updates

--- precache-manifest.605f62d3874e0bb00c72c63cc940f386.js
+++ precache-manifest.605f62d3874e0bb00c72c63cc940f386.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "ab67b44387360b211babab0406a31fb9",
+    "revision": "a3458a04835e45ded7f699aaeb98a504",
     "url": "/quran/index.html"
   },
   {
-    "revision": "919cd863b09f9ddb192b",
-    "url": "/quran/static/css/main.613b74b2.chunk.css"
+    "revision": "0cbda7de0890c140cc99",
+    "url": "/quran/static/css/main.7f04ff31.chunk.css"
   },
   {
     "revision": "13e2a56d7c3193461e29",
     "url": "/quran/static/js/2.6ae190d2.chunk.js"
   },
   {
-    "revision": "919cd863b09f9ddb192b",
-    "url": "/quran/static/js/main.a01d2293.chunk.js"
+    "revision": "0cbda7de0890c140cc99",
+    "url": "/quran/static/js/main.1cde6627.chunk.js"
   },
   {
     "revision": "5eb79085e85e295b3b3e",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/quran/precache-manifest.a3c4abb1f54196c71cf59cabb550690b.js"
+  "/quran/precache-manifest.605f62d3874e0bb00c72c63cc940f386.js"
 );
 
 self.addEventListener('message', (event) => {
