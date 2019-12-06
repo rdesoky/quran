8d98c0eb9c6f87eedcbb2bc9dbaab965dac245d8
Ramy (Home PC) <rdesoky@gmail.com>
2019-12-06 08:25:15 -0800

    Updates

---
+++ precache-manifest.42d2a91fd9345988af2d48360926f156.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2aa68e88e88ef7b377b8741eaeff4e62",
+    "url": "/quran/index.html"
+  },
+  {
+    "revision": "56b4b1df775821cedc32",
+    "url": "/quran/static/css/main.6abd1e6f.chunk.css"
+  },
+  {
+    "revision": "5089d75666bc74100f0e",
+    "url": "/quran/static/js/2.e9f46e3f.chunk.js"
+  },
+  {
+    "revision": "56b4b1df775821cedc32",
+    "url": "/quran/static/js/main.8be13f1d.chunk.js"
+  },
+  {
+    "revision": "5eb79085e85e295b3b3e",
+    "url": "/quran/static/js/runtime~main.72f9a983.js"
+  }
+]);
