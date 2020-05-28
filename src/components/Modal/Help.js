89000d5a0cf8e059701329fd247fe33d7c3dca49
Eldesoky, Ramy <Ramy_Eldesoky@McAfee.com>
2020-05-28 13:37:36 +0200

    Add credits and audio fix instruction in Help

--- src/components/Modal/Help.js
+++ src/components/Modal/Help.js
@@ -6,27 +6,61 @@ import { FormattedMessage as String } from "react-intl";
 import { AppConsumer } from "../../context/App";
 
 const Help = () => {
-    return (
-        <>
-            <div className="Title">
-                <String id="help" />
-            </div>
-            <div className="PopupBody">
-                <String
-                    id="contact_us"
-                    values={{
-                        email: (
-                            <div>
-                                <a href="mailto:webmaster@muslim-web.com">
-                                    webmaster@muslim-web.com
-                                </a>
-                            </div>
-                        )
-                    }}
-                />
-            </div>
-        </>
-    );
+  const [unsecureLink, setUnsecureLink] = useState(null);
+  useEffect(() => {
+    const newLink = window.location.href.replace("https://", "http://");
+    if (newLink !== window.location.href) {
+      setUnsecureLink(newLink);
+    }
+  }, []);
+
+  return (
+    <>
+      <div className="Title">
+        <String id="help" />
+      </div>
+      <div className="PopupBody">
+        <div>
+          <String
+            id="contact_us"
+            values={{
+              email: (
+                <div>
+                  <a href="mailto:webmaster@muslim-web.com">
+                    webmaster@muslim-web.com
+                  </a>
+                </div>
+              ),
+            }}
+          />
+        </div>
+        <hr />
+        <div>
+          <String
+            id="credits"
+            values={{
+              link: (
+                <a href="https://everyayah.com" target="_blank">
+                  everyayah.com
+                </a>
+              ),
+            }}
+          />
+        </div>
+        {unsecureLink && (
+          <div>
+            <hr />
+            <String
+              id="audio_fix"
+              values={{
+                link: <a href={unsecureLink}>{unsecureLink}</a>,
+              }}
+            />
+          </div>
+        )}
+      </div>
+    </>
+  );
 };
 
 export default AppConsumer(Help);
