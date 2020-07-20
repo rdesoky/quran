59c1ea2e4f2db1d79919490f540057e9d5ae817a
Eldesoky, Ramy <Ramy_Eldesoky@McAfee.com>
2020-07-20 12:25:23 +0200

    Updates

---
+++ precache-manifest.9816e187996cf2ad638569f224caa510.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a546172f504a12f6879dfadaacd3991d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "337c949faff1ef9edc92",
+    "url": "/static/css/main.d4cd0750.chunk.css"
+  },
+  {
+    "revision": "145df076bcb0df649360",
+    "url": "/static/js/2.e0968df2.chunk.js"
+  },
+  {
+    "revision": "337c949faff1ef9edc92",
+    "url": "/static/js/main.1eff8a2b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.76f9282b0c2237996ea983abc0650749.js"
+  "/precache-manifest.9816e187996cf2ad638569f224caa510.js"
 );
 
 self.addEventListener('message', (event) => {
