d4429689b92ea7cd5549328f65afcbce6ec35431
Ramy Eldesoky@i5560 <rdesoky@gmail.com>
2025-05-30 15:33:20 -0700

    Optimize side panel spacings. Popups cascading

--- src/components/Message.js
+++ src/components/Message.js
@@ -3,76 +3,76 @@ import { FontAwesomeIcon as Icon } from "@fortawesome/react-fontawesome";
 import { useLayoutEffect, useRef } from "react";
 import { FormattedMessage as String } from "react-intl";
 
-export const Message = ({ msgBoxInfo, onClose, disabled }) => {
-    const yesRef = useRef();
-    const contentRef = useRef();
-    const closeBtnRef = useRef();
+export const Message = ({ msgBoxInfo, onClose, disabled, style }) => {
+	const yesRef = useRef();
+	const contentRef = useRef();
+	const closeBtnRef = useRef();
 
-    useLayoutEffect(() => {
-        if (yesRef.current) {
-            yesRef.current.focus();
-        } else if (contentRef.current) {
-            const btn = contentRef.current.querySelector(
-                "button,input,select,radio"
-            );
-            if (btn) {
-                btn.focus();
-            } else {
-                closeBtnRef.current.focus();
-            }
-        }
-    }, []);
+	useLayoutEffect(() => {
+		if (yesRef.current) {
+			yesRef.current.focus();
+		} else if (contentRef.current) {
+			const btn = contentRef.current.querySelector(
+				"button,input,select,radio"
+			);
+			if (btn) {
+				btn.focus();
+			} else {
+				closeBtnRef.current.focus();
+			}
+		}
+	}, []);
 
-    const onYes = (e) => {
-        if (msgBoxInfo.onYes) {
-            setTimeout(() => {
-                msgBoxInfo.onYes();
-            });
-        }
-        onClose(e);
-    };
+	const onYes = (e) => {
+		if (msgBoxInfo.onYes) {
+			setTimeout(() => {
+				msgBoxInfo.onYes();
+			});
+		}
+		onClose(e);
+	};
 
-    const onNo = (e) => {
-        if (msgBoxInfo.onNo) {
-            setTimeout(() => {
-                msgBoxInfo.onNo();
-            });
-        }
-        onClose(e);
-    };
+	const onNo = (e) => {
+		if (msgBoxInfo.onNo) {
+			setTimeout(() => {
+				msgBoxInfo.onNo();
+			});
+		}
+		onClose(e);
+	};
 
-    return (
-        <div className="MessageBox">
-            <div className="MessageBoxTitle">{msgBoxInfo.title}</div>
-            <button className="CloseButton" onClick={onClose}>
-                <Icon icon={faTimes} />
-            </button>
-            <div className="MessageBoxContent" ref={contentRef}>
-                {msgBoxInfo.content}
-            </div>
-            {msgBoxInfo.onYes && (
-                <div className="ButtonsBar">
-                    <button onClick={onYes} ref={yesRef}>
-                        <String id="yes" />
-                    </button>
-                    <button onClick={onNo}>
-                        <String id="no" />
-                    </button>
-                </div>
-            )}
-            {disabled && (
-                <div
-                    style={{
-                        position: "absolute",
-                        top: 0,
-                        left: 0,
-                        width: "100%",
-                        height: "100%",
-                        opacity: 0.2,
-                        background: "white",
-                    }}
-                />
-            )}
-        </div>
-    );
+	return (
+		<div className="MessageBox" style={style}>
+			<div className="MessageBoxTitle">{msgBoxInfo.title}</div>
+			<button className="CloseButton" onClick={onClose}>
+				<Icon icon={faTimes} />
+			</button>
+			<div className="MessageBoxContent" ref={contentRef}>
+				{msgBoxInfo.content}
+			</div>
+			{msgBoxInfo.onYes && (
+				<div className="ButtonsBar">
+					<button onClick={onYes} ref={yesRef}>
+						<String id="yes" />
+					</button>
+					<button onClick={onNo}>
+						<String id="no" />
+					</button>
+				</div>
+			)}
+			{disabled && (
+				<div
+					style={{
+						position: "absolute",
+						top: 0,
+						left: 0,
+						width: "100%",
+						height: "100%",
+						opacity: 0.2,
+						background: "white",
+					}}
+				/>
+			)}
+		</div>
+	);
 };
--- src/components/Modal/QIndex.js
+++ src/components/Modal/QIndex.js
@@ -1,8 +1,8 @@
 import {
-    faBookmark,
-    faHeart,
-    faListAlt,
-    faTimes,
+	faBookmark,
+	faHeart,
+	faListAlt,
+	faTimes,
 } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon as Icon } from "@fortawesome/react-fontawesome";
 import React, { useEffect, useState } from "react";
@@ -17,142 +17,142 @@ import { HifzRanges } from "../Hifz";
 import { SuraList } from "../SuraList";
 
 export default function QIndex({ simple }) {
-    const lang = useSelector(selectLang);
-    const [keyboard, setKeyboard] = useState(false);
-    const [activeTab, setActiveTab] = useState(
-        localStorage.getItem("activeTab") || "index"
-    );
-    const [filter, setFilter] = useState("");
-    const appHeight = useSelector(selectAppHeight);
-    const bodyRef = useSnapHeightToBottomOf(appHeight - 15);
+	const lang = useSelector(selectLang);
+	const [keyboard, setKeyboard] = useState(false);
+	const [activeTab, setActiveTab] = useState(
+		localStorage.getItem("activeTab") || "index"
+	);
+	const [filter, setFilter] = useState("");
+	const appHeight = useSelector(selectAppHeight);
+	const bodyRef = useSnapHeightToBottomOf(appHeight - 26);
 
-    const selectTab = (tabId) => {
-        localStorage.setItem("activeTab", tabId);
-        setActiveTab(tabId);
-    };
+	const selectTab = (tabId) => {
+		localStorage.setItem("activeTab", tabId);
+		setActiveTab(tabId);
+	};
 
-    let typingConsole;
+	let typingConsole;
 
-    const hideKeyboard = (e) => {
-        setKeyboard(false);
-    };
+	const hideKeyboard = (e) => {
+		setKeyboard(false);
+	};
 
-    useEffect(() => {
-        if (typingConsole) {
-            typingConsole.focus();
-        }
-    }, [typingConsole]);
+	useEffect(() => {
+		if (typingConsole) {
+			typingConsole.focus();
+		}
+	}, [typingConsole]);
 
-    const showKeyboard = (e) => {
-        setKeyboard(true);
-    };
+	const showKeyboard = (e) => {
+		setKeyboard(true);
+	};
 
-    const updateFilter = (filter) => {
-        setFilter(filter);
-    };
+	const updateFilter = (filter) => {
+		setFilter(filter);
+	};
 
-    const clearFilter = (e) => {
-        setFilter("");
-        e.stopPropagation();
-    };
+	const clearFilter = (e) => {
+		setFilter("");
+		e.stopPropagation();
+	};
 
-    if (simple) {
-        return (
-            <>
-                <div className="Title"></div>
-                <div ref={bodyRef} className="PopupBody">
-                    <SuraList simple={simple} />
-                </div>
-            </>
-        );
-    }
+	if (simple) {
+		return (
+			<>
+				<div className="Title"></div>
+				<div ref={bodyRef} className="PopupBody">
+					<SuraList simple={simple} />
+				</div>
+			</>
+		);
+	}
 
-    return (
-        <>
-            <div className="Title">
-                <div className="ButtonsBar">
-                    <button
-                        onClick={(e) => selectTab("index")}
-                        className={"CommandButton".appendWord(
-                            "active",
-                            activeTab === "index"
-                        )}
-                    >
-                        <Icon icon={faListAlt} />
-                        <span>
-                            <Message id="index" />
-                        </span>
-                    </button>
-                    <button
-                        onClick={(e) => selectTab("hifz")}
-                        className={"CommandButton".appendWord(
-                            "active",
-                            activeTab === "hifz"
-                        )}
-                    >
-                        <Icon icon={faHeart} />
-                        <span>
-                            <Message id="favorites" />
-                        </span>
-                    </button>
-                    <button
-                        onClick={(e) => selectTab("bookmarks")}
-                        className={"CommandButton".appendWord(
-                            "active",
-                            activeTab === "bookmarks"
-                        )}
-                    >
-                        <Icon icon={faBookmark} />
-                        <span>
-                            <Message id="bookmarks" />
-                        </span>
-                    </button>
-                </div>
-            </div>
-            <div
-                className={"TypingConsole" + (!filter.length ? " empty" : "")}
-                ref={(ref) => {
-                    typingConsole = ref;
-                }}
-                tabIndex="0"
-                onClick={showKeyboard}
-            >
-                {filter || <Message id="find_sura" />}
-                {filter ? (
-                    <div className="ClearButton" onClick={clearFilter}>
-                        <Icon icon={faTimes} />
-                    </div>
-                ) : (
-                    ""
-                )}
-            </div>
+	return (
+		<>
+			<div className="Title">
+				<div className="ButtonsBar">
+					<button
+						onClick={(e) => selectTab("index")}
+						className={"CommandButton".appendWord(
+							"active",
+							activeTab === "index"
+						)}
+					>
+						<Icon icon={faListAlt} />
+						<span>
+							<Message id="index" />
+						</span>
+					</button>
+					<button
+						onClick={(e) => selectTab("hifz")}
+						className={"CommandButton".appendWord(
+							"active",
+							activeTab === "hifz"
+						)}
+					>
+						<Icon icon={faHeart} />
+						<span>
+							<Message id="favorites" />
+						</span>
+					</button>
+					<button
+						onClick={(e) => selectTab("bookmarks")}
+						className={"CommandButton".appendWord(
+							"active",
+							activeTab === "bookmarks"
+						)}
+					>
+						<Icon icon={faBookmark} />
+						<span>
+							<Message id="bookmarks" />
+						</span>
+					</button>
+				</div>
+			</div>
+			<div
+				className={"TypingConsole" + (!filter.length ? " empty" : "")}
+				ref={(ref) => {
+					typingConsole = ref;
+				}}
+				tabIndex="0"
+				onClick={showKeyboard}
+			>
+				{filter || <Message id="find_sura" />}
+				{filter ? (
+					<div className="ClearButton" onClick={clearFilter}>
+						<Icon icon={faTimes} />
+					</div>
+				) : (
+					""
+				)}
+			</div>
 
-            <AKeyboard
-                style={{ display: keyboard ? "block" : "none" }}
-                initText={filter}
-                onUpdateText={updateFilter}
-                onEnter={hideKeyboard}
-                onCancel={hideKeyboard}
-                lang={lang}
-            />
+			<AKeyboard
+				style={{ display: keyboard ? "block" : "none" }}
+				initText={filter}
+				onUpdateText={updateFilter}
+				onEnter={hideKeyboard}
+				onCancel={hideKeyboard}
+				lang={lang}
+			/>
 
-            <div
-                className="PopupBody"
-                ref={bodyRef}
-                onTouchStart={hideKeyboard}
-                onMouseDown={hideKeyboard}
-            >
-                {activeTab === "index" ? (
-                    <SuraList filter={filter} trigger="indices_chapters" />
-                ) : activeTab === "hifz" ? (
-                    <HifzRanges filter={filter} trigger="indices_hifz" />
-                ) : (
-                    <BookmarksList
-                        filter={filter}
-                        trigger="indices_bookmarks"
-                    />
-                )}
-            </div>
-        </>
-    );
+			<div
+				className="PopupBody"
+				ref={bodyRef}
+				onTouchStart={hideKeyboard}
+				onMouseDown={hideKeyboard}
+			>
+				{activeTab === "index" ? (
+					<SuraList filter={filter} trigger="indices_chapters" />
+				) : activeTab === "hifz" ? (
+					<HifzRanges filter={filter} trigger="indices_hifz" />
+				) : (
+					<BookmarksList
+						filter={filter}
+						trigger="indices_bookmarks"
+					/>
+				)}
+			</div>
+		</>
+	);
 }
