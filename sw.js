b7bf5b26851265c4b12ffb002e50a2a54e98731e
Ramy Eldesoky@i5560 <rdesoky@gmail.com>
2025-05-30 15:39:41 -0700

    Updates

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
-//Sun May 07 2023 06:29:19 GMT-0700 (Pacific Daylight Time)
+//Fri May 30 2025 15:39:27 GMT-0700 (Pacific Daylight Time)
 /* eslint-disable no-restricted-globals */
 
-const appVersion = 18;
+const appVersion = 19;
 const appCacheId = `app.v${appVersion}`;
 const assetsVersion = 1;
 const assetsCacheId = `assets.v${assetsVersion}`;
@@ -10,120 +10,120 @@ const assetRoots = [location.origin /*, "https://www.everyayah.com/data"*/];
 const cacheExcludes = ["browser-sync"]; //
 
 const deleteOldCaches = async () => {
-    const cacheNames = await caches.keys();
-    const oldCacheNames = cacheNames.filter(
-        (name) => name !== appCacheId && name !== assetsCacheId
-    );
-    await Promise.all(oldCacheNames.map((name) => caches.delete(name)));
+	const cacheNames = await caches.keys();
+	const oldCacheNames = cacheNames.filter(
+		(name) => name !== appCacheId && name !== assetsCacheId
+	);
+	await Promise.all(oldCacheNames.map((name) => caches.delete(name)));
 };
 
 const addResourcesToCache = async () => {
-    const appManifest = await fetch("asset-manifest.json").then((res) =>
-        res.json()
-    );
-    const appFiles = Object.values(appManifest.files).filter(
-        (file) => !file.includes(".map")
-    );
-    const appCache = await caches.open(appCacheId);
-    await appCache.addAll(appFiles);
+	const appManifest = await fetch("asset-manifest.json").then((res) =>
+		res.json()
+	);
+	const appFiles = Object.values(appManifest.files).filter(
+		(file) => !file.includes(".map")
+	);
+	const appCache = await caches.open(appCacheId);
+	await appCache.addAll(appFiles);
 
-    const publicManifest = await fetch("public-manifest.json").then((res) =>
-        res.json()
-    );
-    const publicAssetsFiles = publicManifest.files.filter(
-        (name) => !name.includes("sw.js")
-    );
-    const assetCache = await caches.open(assetsCacheId);
-    await assetCache.addAll(publicAssetsFiles);
+	const publicManifest = await fetch("public-manifest.json").then((res) =>
+		res.json()
+	);
+	const publicAssetsFiles = publicManifest.files.filter(
+		(name) => !name.includes("sw.js")
+	);
+	const assetCache = await caches.open(assetsCacheId);
+	await assetCache.addAll(publicAssetsFiles);
 };
 
 const putInCache = async (request, response) => {
-    if (
-        request.method !== "GET" ||
-        response.status !== 200 ||
-        !assetRoots.some((cacheBase) => request.url.startsWith(cacheBase))
-    ) {
-        return;
-    }
-    if (cacheExcludes.some((exclude) => request.url.includes(exclude))) {
-        return;
-    }
-    const assetCache = await caches.open(assetsCacheId);
-    await assetCache.put(request, response);
+	if (
+		request.method !== "GET" ||
+		response.status !== 200 ||
+		!assetRoots.some((cacheBase) => request.url.startsWith(cacheBase))
+	) {
+		return;
+	}
+	if (cacheExcludes.some((exclude) => request.url.includes(exclude))) {
+		return;
+	}
+	const assetCache = await caches.open(assetsCacheId);
+	await assetCache.put(request, response);
 };
 
 const cacheFirst = async ({ request, preloadResponsePromise, fallbackUrl }) => {
-    // First try to get the resource from the cache
-    const responseFromCache = await caches.match(request);
-    if (responseFromCache && responseFromCache.status === 200) {
-        return responseFromCache;
-    }
+	// First try to get the resource from the cache
+	const responseFromCache = await caches.match(request);
+	if (responseFromCache && responseFromCache.status === 200) {
+		return responseFromCache;
+	}
 
-    // Next try to use the preloaded response, if it's there
-    if (preloadResponsePromise) {
-        const preloadResponse = await preloadResponsePromise;
-        if (preloadResponse) {
-            console.info("using preload response", preloadResponse);
-            putInCache(request, preloadResponse.clone());
-            return preloadResponse;
-        }
-    }
+	// Next try to use the preloaded response, if it's there
+	if (preloadResponsePromise) {
+		const preloadResponse = await preloadResponsePromise;
+		if (preloadResponse) {
+			console.info("using preload response", preloadResponse);
+			putInCache(request, preloadResponse.clone());
+			return preloadResponse;
+		}
+	}
 
-    // Next try to get the resource from the network
-    try {
-        const responseFromNetwork = await fetch(request);
-        // response may be used only once
-        // we need to save clone to put one copy in cache
-        // and serve second one
-        putInCache(request, responseFromNetwork.clone());
-        return responseFromNetwork;
-    } catch (error) {
-        const fallbackResponse = await caches.match(fallbackUrl);
-        if (fallbackResponse) {
-            return fallbackResponse;
-        }
-        // when even the fallback response is not available,
-        // there is nothing we can do, but we must always
-        // return a Response object
-        return new Response("Network error happened", {
-            status: 408,
-            headers: { "Content-Type": "text/plain" },
-        });
-    }
+	// Next try to get the resource from the network
+	try {
+		const responseFromNetwork = await fetch(request);
+		// response may be used only once
+		// we need to save clone to put one copy in cache
+		// and serve second one
+		putInCache(request, responseFromNetwork.clone());
+		return responseFromNetwork;
+	} catch (error) {
+		const fallbackResponse = await caches.match(fallbackUrl);
+		if (fallbackResponse) {
+			return fallbackResponse;
+		}
+		// when even the fallback response is not available,
+		// there is nothing we can do, but we must always
+		// return a Response object
+		return new Response("Network error happened", {
+			status: 408,
+			headers: { "Content-Type": "text/plain" },
+		});
+	}
 };
 
 const enableNavigationPreload = async () => {
-    if (self.registration.navigationPreload) {
-        // Enable navigation preloads!
-        await self.registration.navigationPreload.enable();
-    }
+	if (self.registration.navigationPreload) {
+		// Enable navigation preloads!
+		await self.registration.navigationPreload.enable();
+	}
 };
 
 self.addEventListener("activate", (event) => {
-    event.waitUntil(
-        Promise.all([enableNavigationPreload(), deleteOldCaches()])
-    );
+	event.waitUntil(
+		Promise.all([enableNavigationPreload(), deleteOldCaches()])
+	);
 });
 
 self.addEventListener("install", (event) => {
-    const cacheResources = addResourcesToCache();
-    event.waitUntil(cacheResources);
-    cacheResources.then(() => {
-        self.skipWaiting();
-    });
+	const cacheResources = addResourcesToCache();
+	event.waitUntil(cacheResources);
+	cacheResources.then(() => {
+		self.skipWaiting();
+	});
 });
 
 self.addEventListener("fetch", (event) => {
-    const request =
-        event.request.mode === "navigate"
-            ? new Request(location.origin + "/index.html")
-            : event.request;
+	const request =
+		event.request.mode === "navigate"
+			? new Request(location.origin + "/index.html")
+			: event.request;
 
-    event.respondWith(
-        cacheFirst({
-            request: request,
-            preloadResponsePromise: event.preloadResponse,
-            // fallbackUrl: "/images/offline.png",
-        })
-    );
+	event.respondWith(
+		cacheFirst({
+			request: request,
+			preloadResponsePromise: event.preloadResponse,
+			// fallbackUrl: "/images/offline.png",
+		})
+	);
 });
