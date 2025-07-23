5cc53b297eb6f9043501f471806719c0ee954dc5
Ramy Eldesoky@i5560 <rdesoky@gmail.com>
2025-07-22 22:59:07 -0700

    Refactor PageHeader component: streamline imports, enhance layout, and integrate CircleProgress

--- src/components/Pager/PageHeader.tsx
+++ src/components/Pager/PageHeader.tsx
@@ -1,47 +1,66 @@
 import {
-    ayaID,
-    ayaIdInfo,
-    ayaIdPage,
-    getPageSuraIndex,
+	getPagePartNumber,
+	getPageSuraIndex,
+	TOTAL_PAGES
 } from "@/services/qData";
 import {
-    PAGE_HEADER_HEIGHT,
-    selectActivePage,
-    selectShownPages,
+	PAGE_HEADER_HEIGHT,
+	selectActivePage,
+	selectShownPages,
 } from "@/store/layoutSlice";
-import { selectStartSelection } from "@/store/navSlice";
 import { useMemo } from "react";
 import { useSelector } from "react-redux";
 import SuraName from "../SuraName";
+import { CircleProgress } from "../CircleProgress";
+import { useIntl } from "react-intl";
 
 type PageHeaderProps = {
-    order: 0 | 1; // 0 for right page, 1 for left page
+	order: 0 | 1; // 0 for right page, 1 for left page
 };
 
 export function PageHeader({ order }: PageHeaderProps) {
-    const shownPages = useSelector(selectShownPages);
-    const activePage = useSelector(selectActivePage);
-    const pageIndex = shownPages[order];
-    const suraIndex = useMemo(
-        () => getPageSuraIndex(pageIndex + 1),
-        [pageIndex]
-    );
+	const shownPages = useSelector(selectShownPages);
+	const activePage = useSelector(selectActivePage);
+	const pageIndex = shownPages[order];
+	const suraIndex = useMemo(
+		() => getPageSuraIndex(pageIndex + 1),
+		[pageIndex]
+	);
+	const partIndex = useMemo(
+		() => getPagePartNumber(pageIndex + 1) - 1,
+		[pageIndex]
+	);
+	const intl = useIntl();
 
-    return (
-        <div
-            className={"PageHeader".appendWord(
-                "active",
-                pageIndex === activePage
-            )}
-            style={{
-                height: PAGE_HEADER_HEIGHT,
-                padding: "0 10px",
-                boxSizing: "border-box",
-            }}
-        >
-            {/* <div className="PageHeaderContent"> */}
-            <SuraName index={suraIndex} />
-            {/* </div> */}
-        </div>
-    );
+
+	return (
+		<div
+			className={"PageHeader".appendWord(
+				"active",
+				pageIndex === activePage
+			)}
+			style={{
+				height: PAGE_HEADER_HEIGHT,
+				// padding: "0 10px",
+				boxSizing: "border-box",
+				borderRadius: 12
+			}}
+		>
+			<CircleProgress
+				target={TOTAL_PAGES}
+				progress={pageIndex + 1}
+				display={partIndex + 1}
+				// onClick={showPartContextPopup}
+				strokeWidth={3}
+				title={intl.formatMessage(
+					{ id: "part_num" },
+					{ num: partIndex + 1 }
+				)}
+				style={{ margin: 1 }}
+			/>
+
+			<span style={{ padding: "0 10px" }}><SuraName index={suraIndex} /></span>
+
+		</div>
+	);
 }
