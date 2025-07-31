39f0b8112f8e427f56fac75f6db1dddcdfe3f1c5
Ramy Eldesoky@i5560 <rdesoky@gmail.com>
2025-07-31 04:16:36 -0700

    Don't rely on Vite to create the manifest files

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
 import tsconfigPaths from "vite-tsconfig-paths";
-import { VitePWA } from "vite-plugin-pwa";
+// import { VitePWA } from "vite-plugin-pwa";
 import { resolve } from "path";
 
 // https://vitejs.dev/config/
@@ -16,48 +16,48 @@ export default defineConfig(({ mode }) => {
 			react(),
 			svgr(),
 			tsconfigPaths(),
-			VitePWA({
-				registerType: "autoUpdate",
-				// Include public assets that need to be available offline
-				includeAssets: [
-					"favicon.ico",
-					"robots.txt",
-					"images/**/*",
-					"pg_map/**/*",
-					"translation/**/*",
-					"db/**/*",
-				],
-				// Use the custom service worker file instead of generating one
-				injectRegister: null,
-				strategies: "injectManifest",
-				injectManifest: {
-					injectionPoint: undefined,
-					swSrc: "./public/sw.js",
-					swDest: "./build/sw.js",
-					// Customize as needed
-					globDirectory: "./build",
-					globPatterns: [
-						"**/*.{js,css,html,ico,png,svg,jpg,jpeg,json,txt}",
-					],
-				},
-				manifest: {
-					name: "Quran Hafiz",
-					short_name: "Hafiz",
-					description:
-						"Quran Application with multiple translations and reciters",
-					theme_color: "#ffffff",
-					start_url: "/",
-					display: "standalone",
-					background_color: "#ffffff",
-					icons: [
-						{
-							src: "/images/quran_hafiz.png",
-							sizes: "192x192",
-							type: "image/png",
-						},
-					],
-				},
-			}),
+			// VitePWA({
+			// 	registerType: "autoUpdate",
+			// 	// Include public assets that need to be available offline
+			// 	includeAssets: [
+			// 		"favicon.ico",
+			// 		"robots.txt",
+			// 		"images/**/*",
+			// 		"pg_map/**/*",
+			// 		"translation/**/*",
+			// 		"db/**/*",
+			// 	],
+			// 	// Use the custom service worker file instead of generating one
+			// 	injectRegister: null,
+			// 	strategies: "injectManifest",
+			// 	injectManifest: {
+			// 		injectionPoint: undefined,
+			// 		swSrc: "./public/sw.js",
+			// 		swDest: "./build/sw.js",
+			// 		// Customize as needed
+			// 		globDirectory: "./build",
+			// 		globPatterns: [
+			// 			"**/*.{js,css,html,ico,png,svg,jpg,jpeg,json,txt}",
+			// 		],
+			// 	},
+			// 	manifest: {
+			// 		name: "Quran Hafiz",
+			// 		short_name: "Hafiz",
+			// 		description:
+			// 			"Quran Application with multiple translations and reciters",
+			// 		theme_color: "#ffffff",
+			// 		start_url: "/",
+			// 		display: "standalone",
+			// 		background_color: "#ffffff",
+			// 		icons: [
+			// 			{
+			// 				src: "/images/quran_hafiz.png",
+			// 				sizes: "192x192",
+			// 				type: "image/png",
+			// 			},
+			// 		],
+			// 	},
+			// }),
 		],
 		resolve: {
 			alias: {
