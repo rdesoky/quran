e1482678f5c301b8d7fb74d57f344b9bc46ed700
Ramy Eldesoky@i5560 <rdesoky@gmail.com>
2025-07-31 04:16:01 -0700

    post-build to create app-manifest

--- sw-post-build.mjs
+++ sw-post-build.mjs
@@ -28,10 +28,19 @@ const db = fs
 	.readdirSync(path.join("public", "db"))
 	.map((file) => `/db/${file}`);
 
-const manifest = {
+const assetsManifest = {
 	files: pg_map.concat(images).concat(translation).concat(db),
 };
 
 const manifestPath = path.join("build", "public-manifest.json");
 //Injecting timestamp into the service worker file
-fs.writeFileSync(manifestPath, JSON.stringify(manifest, null, 4));
+fs.writeFileSync(manifestPath, JSON.stringify(assetsManifest, null, 4));
+
+const appManifestPath = path.join("build", "app-manifest.json");
+const appFiles = fs.readdirSync(path.join("build","assets")).map(file => `/assets/${file}`);
+appFiles.push("/sw.js", "/index.html", "/404.html");
+fs.writeFileSync(appManifestPath, JSON.stringify({ files: appFiles }, null, 4));
+
+console.log(`** Service worker built at ${swBuildPath}`);
+console.log(`** Public manifest built at ${manifestPath}`);
+console.log(`** App manifest built at ${appManifestPath}`);
